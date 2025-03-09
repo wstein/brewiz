3ac605fe1782eac16269eca32e0404e17a0673f7
Werner Stein <werner.stein@gmail.com>
2025-03-09 10:56:43 +0100

    Use relative paths for assets in index.html and update Vite config base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { readFileSync } from 'fs';
 const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
+  base: './',
   plugins: [solid()],
   server: {
     port: 8048,
